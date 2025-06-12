07793f8d6c381713c1e66410ef9b52ec26cb20ef
Michael Yuen <michael@overimagined.com>
2025-06-12 17:40:56 +0800

    Added 05_WindowSurface

---
+++ 05_WindowSurface/Program.cs
@@ -0,0 +1,344 @@
+﻿using Silk.NET.Core;
+using Silk.NET.Core.Native;
+using Silk.NET.Maths;
+using Silk.NET.Vulkan;
+using Silk.NET.Vulkan.Extensions.EXT;
+using Silk.NET.Vulkan.Extensions.KHR;
+using Silk.NET.Windowing;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace VulkanSilk;
+
+
+unsafe class HelloTriangleApplication
+{
+#if DEBUG
+    const bool enableValidationLayers = true;
+#else
+    const bool enableValidationLayers = false;
+#endif
+
+    public int Width = 800;
+    public int Height = 600;
+
+    string[] validationLayers = ["VK_LAYER_KHRONOS_validation"];
+
+    IWindow window;
+
+    Vk vk;
+    Instance instance;
+
+    ExtDebugUtils debugUtils;
+    DebugUtilsMessengerEXT debugMessenger;
+
+    PhysicalDevice physicalDevice;
+    Device device;
+    Queue graphicsQueue;
+    Queue presentQueue;
+
+    KhrSurface khrSurface;
+    SurfaceKHR surface;
+
+    public void Run()
+    {
+        InitWindow();
+        InitVulkan();
+        MainLoop();
+        Cleanup();
+    }
+
+    void InitWindow()
+    {
+        WindowOptions options = WindowOptions.DefaultVulkan with
+        {
+            Size = new Vector2D<int>(Width, Height),
+            WindowBorder = WindowBorder.Resizable,
+        };
+
+        window = Window.Create(options);
+        window.Initialize();
+    }
+
+    void InitVulkan()
+    {
+        CreateInstance();
+        SetupDebugMessenger();
+        CreateSurface();
+        PickPhysicalDevice();
+        CreateLogicalDevice();
+    }
+
+    void MainLoop()
+    {
+        window.Run();
+    }
+
+    void Cleanup()
+    {
+        vk.DestroyDevice(device, null);
+
+        if (enableValidationLayers)
+            debugUtils.DestroyDebugUtilsMessenger(instance, debugMessenger, null);
+
+        khrSurface.DestroySurface(instance, surface, null);
+        vk.DestroyInstance(instance, null);
+        vk.Dispose();
+        window.Dispose();
+    }
+
+    void CreateInstance()
+    {
+        vk = Vk.GetApi();
+
+        if (enableValidationLayers && !CheckValidationLayerSupport())
+            throw new Exception("Validation layer not available");
+
+
+        var appInfo = new ApplicationInfo()
+        {
+            SType = StructureType.ApplicationInfo,
+
+            PApplicationName = (byte*)SilkMarshal.StringToPtr("Hello Triangle"),
+            ApplicationVersion = new Version32(1, 0, 0),
+            PEngineName = (byte*)SilkMarshal.StringToPtr("No Engine"),
+            EngineVersion = new Version32(1, 0, 0),
+            ApiVersion = Vk.Version12
+        };
+
+        var createInfo = new InstanceCreateInfo()
+        {
+            SType = StructureType.InstanceCreateInfo,
+            PApplicationInfo = &appInfo
+        };
+
+        var extensions = GetRequiredExtensions();
+        createInfo.EnabledExtensionCount = (uint)extensions.Length;
+        createInfo.PpEnabledExtensionNames = (byte**)SilkMarshal.StringArrayToPtr(extensions);
+
+        if (enableValidationLayers)
+        {
+            createInfo.EnabledLayerCount = (uint)validationLayers.Length;
+            createInfo.PpEnabledLayerNames = (byte**)SilkMarshal.StringArrayToPtr(validationLayers);
+
+            var debugCreateInfo = new DebugUtilsMessengerCreateInfoEXT();
+            PopulateDebugMessengerCreateInfo(ref debugCreateInfo);
+            createInfo.PNext = &debugCreateInfo;
+        }
+        else
+        {
+            createInfo.EnabledLayerCount = 0;
+            createInfo.PNext = null;
+        }
+
+        if (vk.CreateInstance(in createInfo, null, out instance) != Result.Success)
+            throw new Exception("failed to create instance!");
+
+        SilkMarshal.FreeString((IntPtr)appInfo.PApplicationName);
+        SilkMarshal.FreeString((IntPtr)appInfo.PEngineName);
+        SilkMarshal.Free((nint)createInfo.PpEnabledExtensionNames);
+
+        if (enableValidationLayers)
+            SilkMarshal.Free((nint)createInfo.PpEnabledLayerNames);
+    }
+
+    void PopulateDebugMessengerCreateInfo(ref DebugUtilsMessengerCreateInfoEXT createInfo)
+    {
+        createInfo.SType = StructureType.DebugUtilsMessengerCreateInfoExt;
+
+        createInfo.MessageSeverity = DebugUtilsMessageSeverityFlagsEXT.VerboseBitExt |
+                                     DebugUtilsMessageSeverityFlagsEXT.WarningBitExt |
+                                     DebugUtilsMessageSeverityFlagsEXT.ErrorBitExt;
+
+        createInfo.MessageType = DebugUtilsMessageTypeFlagsEXT.GeneralBitExt |
+                                 DebugUtilsMessageTypeFlagsEXT.PerformanceBitExt |
+                                 DebugUtilsMessageTypeFlagsEXT.ValidationBitExt;
+
+        createInfo.PfnUserCallback = (DebugUtilsMessengerCallbackFunctionEXT)DebugCallback;
+    }
+
+    void SetupDebugMessenger()
+    {
+        if (!enableValidationLayers) return;
+        if (!vk.TryGetInstanceExtension(instance, out debugUtils)) return;
+
+        DebugUtilsMessengerCreateInfoEXT createInfo = new();
+        PopulateDebugMessengerCreateInfo(ref createInfo);
+
+        if (debugUtils.CreateDebugUtilsMessenger(instance, in createInfo, null, out debugMessenger) != Result.Success)
+            throw new Exception("failed to set up debug messenger!");
+    }
+
+    void CreateSurface()
+    {
+        if (!vk.TryGetInstanceExtension(instance, out khrSurface))
+            throw new NotSupportedException("KHR_surface extension not found.");
+
+        surface = window.VkSurface.Create<AllocationCallbacks>(instance.ToHandle(), null).ToSurface();
+    }
+
+    void PickPhysicalDevice()
+    {
+        var devices = vk.GetPhysicalDevices(instance);
+
+        foreach (var device in devices)
+            if (IsDeviceSuitable(device))
+            {
+                physicalDevice = device;
+                break;
+            }
+
+        if (physicalDevice.Handle == 0)
+            throw new Exception("failed to find a suitable GPU!");
+    }
+
+    bool IsDeviceSuitable(PhysicalDevice device)
+    {
+        var indices = FindQueueFamilies(device);
+
+        //get device name
+        PhysicalDeviceProperties properties;
+        vk.GetPhysicalDeviceProperties(device, &properties);
+        Console.Write(SilkMarshal.PtrToString((IntPtr)properties.DeviceName));
+
+        return indices.IsComplete();
+    }
+
+    QueueFamilyIndices FindQueueFamilies(PhysicalDevice device)
+    {
+        var indices = new QueueFamilyIndices();
+
+        uint queueFamilityCount = 0;
+        vk.GetPhysicalDeviceQueueFamilyProperties(device, ref queueFamilityCount, null);
+
+        var queueFamilies = new QueueFamilyProperties[queueFamilityCount];
+        fixed (QueueFamilyProperties* queueFamiliesPtr = queueFamilies)
+            vk.GetPhysicalDeviceQueueFamilyProperties(device, ref queueFamilityCount, queueFamiliesPtr);
+
+
+        uint i = 0;
+        foreach (var queueFamily in queueFamilies)
+        {
+            if (queueFamily.QueueFlags.HasFlag(QueueFlags.GraphicsBit))
+                indices.GraphicsFamily = i;
+
+            khrSurface.GetPhysicalDeviceSurfaceSupport(device, i, surface, out var presentSupport);
+
+            if (presentSupport)
+                indices.PresentFamily = i;
+
+            if (indices.IsComplete())
+                break;
+
+            i++;
+        }
+
+        return indices;
+    }
+
+    string[] GetRequiredExtensions()
+    {
+        var glfwExtensions = window.VkSurface.GetRequiredExtensions(out var glfwExtensionCount);
+        var extensions = SilkMarshal.PtrToStringArray((nint)glfwExtensions, (int)glfwExtensionCount);
+
+        if (enableValidationLayers)
+            return extensions.Append(ExtDebugUtils.ExtensionName).ToArray();
+
+        return extensions;
+    }
+
+    bool CheckValidationLayerSupport()
+    {
+        uint layerCount = 0;
+        vk.EnumerateInstanceLayerProperties(ref layerCount, null);
+
+        var availableLayers = new LayerProperties[layerCount];
+        fixed (LayerProperties* availableLayersPtr = availableLayers)
+            vk.EnumerateInstanceLayerProperties(ref layerCount, availableLayersPtr);
+
+        var availableLayerNames = availableLayers.Select(layer => SilkMarshal.PtrToString((IntPtr)layer.LayerName)).ToHashSet();
+
+        return validationLayers.All(availableLayerNames.Contains);
+    }
+
+    uint DebugCallback(DebugUtilsMessageSeverityFlagsEXT messageSeverity, DebugUtilsMessageTypeFlagsEXT messageTypes, DebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData)
+    {
+        Console.WriteLine($"[validation layer] " + Marshal.PtrToStringAnsi((nint)pCallbackData->PMessage));
+
+        return Vk.False;
+    }
+
+    void CreateLogicalDevice()
+    {
+        var indices = FindQueueFamilies(physicalDevice);
+
+        var uniqueQueueFamilies = new[] { indices.GraphicsFamily!.Value, indices.PresentFamily!.Value };
+        uniqueQueueFamilies = uniqueQueueFamilies.Distinct().ToArray();
+
+        using var mem = GlobalMemory.Allocate(uniqueQueueFamilies.Length * sizeof(DeviceQueueCreateInfo));
+        var queueCreateInfos = (DeviceQueueCreateInfo*)Unsafe.AsPointer(ref mem.GetPinnableReference());
+
+        float queuePriority = 1.0f;
+        for (int i = 0; i < uniqueQueueFamilies.Length; i++)
+        {
+            queueCreateInfos[i] = new()
+            {
+                SType = StructureType.DeviceQueueCreateInfo,
+                QueueFamilyIndex = uniqueQueueFamilies[i],
+                QueueCount = 1,
+                PQueuePriorities = &queuePriority
+            };
+        }
+
+        PhysicalDeviceFeatures deviceFeatures = new();
+
+        DeviceCreateInfo createInfo = new()
+        {
+            SType = StructureType.DeviceCreateInfo,
+            QueueCreateInfoCount = (uint)uniqueQueueFamilies.Length,
+            PQueueCreateInfos = queueCreateInfos,
+            PEnabledFeatures = &deviceFeatures,
+            EnabledExtensionCount = 0
+        };
+
+        if (enableValidationLayers)
+        {
+            createInfo.EnabledLayerCount = (uint)validationLayers.Length;
+            createInfo.PpEnabledLayerNames = (byte**)SilkMarshal.StringArrayToPtr(validationLayers);
+        }
+        else
+        {
+            createInfo.EnabledLayerCount = 0;
+        }
+
+        if (vk.CreateDevice(physicalDevice, in createInfo, null, out device) != Result.Success)
+            throw new Exception("failed to create logical device!");
+
+        vk.GetDeviceQueue(device, indices.GraphicsFamily!.Value, 0, out graphicsQueue);
+        vk.GetDeviceQueue(device, indices.PresentFamily!.Value, 0, out presentQueue);
+
+        if (enableValidationLayers)
+            SilkMarshal.Free((nint)createInfo.PpEnabledLayerNames);
+    }
+
+}
+
+struct QueueFamilyIndices
+{
+    public uint? GraphicsFamily { get; set; }
+    public uint? PresentFamily { get; set; }
+
+    public bool IsComplete()
+    {
+        return GraphicsFamily.HasValue && PresentFamily.HasValue;
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var app = new HelloTriangleApplication();
+        app.Run();
+    }
+}
